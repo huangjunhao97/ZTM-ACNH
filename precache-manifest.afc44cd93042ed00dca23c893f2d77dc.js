1626af6c7bfe22375c366bebb5fb90c46f4d11d3
huangjunhao97 <1424675476@qq.com>
2020-05-21 16:38:07 -0400

    Updates

---
+++ precache-manifest.afc44cd93042ed00dca23c893f2d77dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e1af01a8d730e4ccec2537237f016a",
+    "url": "/ZTM-ACNH/index.html"
+  },
+  {
+    "revision": "858f72f11f1cd47ecf95",
+    "url": "/ZTM-ACNH/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "14560a4efb78f64c6a23",
+    "url": "/ZTM-ACNH/static/css/main.485f4dc3.chunk.css"
+  },
+  {
+    "revision": "858f72f11f1cd47ecf95",
+    "url": "/ZTM-ACNH/static/js/2.2c5cf23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZTM-ACNH/static/js/2.2c5cf23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14560a4efb78f64c6a23",
+    "url": "/ZTM-ACNH/static/js/main.840f46f2.chunk.js"
+  },
+  {
+    "revision": "3648da30644c65f4cd54",
+    "url": "/ZTM-ACNH/static/js/runtime-main.4bccfdba.js"
+  },
+  {
+    "revision": "ab1dabbd8ffd289a5c35cb151879e987",
+    "url": "/ZTM-ACNH/static/media/Arvo-Bold.ab1dabbd.ttf"
+  }
+]);
